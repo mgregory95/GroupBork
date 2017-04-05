15e5d86d3f67ed1c5e9f852b938f15d4db4e732f
mgregory95 <mgregory@umw.edu>
2017-04-05 13:08:12 -0400

    Update Exit.java

--- Exit.java
+++ Exit.java
@@ -1,6 +1,7 @@
 
 package zeitz_borkv3;
 
+
 import java.util.Scanner;
 
 public class Exit {
@@ -50,6 +51,29 @@ public class Exit {
                 Dungeon.SECOND_LEVEL_DELIM + "' after exit.");
         }
     }
+    
+    
+    //Another Exit Constructor     GROUP BORK
+    
+    /** Given a Scanner object positioned at the beginning of an "exit" file
+        entry, read and return an Exit object representing it. 
+        @param d The dungeon that contains this exit (so that Room objects 
+        may be obtained.)
+        * @param s The scanner object that will read through the file. 
+        * @param visibility  this boolean allows the program to know is the exit
+        * is hidden or not. The only place this will be true is for the 
+        * hidden exit of which there will only be two. Every other exit will 
+        * have a negative value for this boolean. 
+        @throws NoExitException The reader object is not positioned at the
+        start of an exit entry. A side effect of this is the reader's cursor
+        is now positioned one line past where it was.
+        @throws IllegalDungeonFormatException A structural problem with the
+        dungeon file itself, detected when trying to read this room.
+     */
+    Exit(Scanner s, Dungeon d, boolean visibility) throws NoExitException,
+        Dungeon.IllegalDungeonFormatException {
+        
+    }
 
     // Common object initialization tasks.
     private void init() {
@@ -62,4 +86,18 @@ public class Exit {
     String getDir() { return dir; }
     Room getSrc() { return src; }
     Room getDest() { return dest; }
+    
+    
+ /**
+ * unlock().
+ * This method is a getter for the visibility instance variable. This allows you 
+ * access to this variable. 
+ * @return boolean visibility     visibility is a boolean that states whether the 
+ *                                  exit is hidden or not. 
+ * @author Meredith
+ * 
+ */
+    boolean getVisibility(){
+        
+    }
 }
