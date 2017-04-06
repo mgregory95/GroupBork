43f927e105a84177eb4afdc4f9c3c81f375ec613
mgregory95 <mgregory@umw.edu>
2017-04-05 23:27:15 -0400

    Update Win.java

--- Win.java
+++ Win.java
@@ -10,7 +10,7 @@ public class Win extends Event {
  /**
  * This is the Win constructor. When the adventurer wins a win event is created. 
  *
- * @author Ava Monsouri
+ * @author Ava 
  */
     public Win(){
         
@@ -19,7 +19,7 @@ public class Win extends Event {
 * This is the execute method that the Win class must have after extending Event - execute alters the
 * game to close it down and produces a WINNER! message.
 * @return A String letting the player know that they won
-* @author Ava Monsouri
+* @author Ava
 */
     
     public String execute(){
