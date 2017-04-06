e4ced910a81cdd4ec99576124db9f7266c445f12
mgregory95 <mgregory@umw.edu>
2017-04-05 23:26:29 -0400

    Update Transform.java

--- Transform.java
+++ Transform.java
@@ -1,24 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package borkgroupeventclass;
 
+
 /**
- *
- * @author Ava Mansouri
- */
+* The transform class is an event class. This class extends the abstract class Event. 
+* This class produces a message and alters an item when called. 
+*/
 public class Transform extends Event {
     
     private Item p; 
     
-    //@param item p is the Item that needs to be transformed
+ /**
+ * This is the Transform constructor. When an item is transformed a transform event is created. 
+ * @param p  an item that is to be transformed
+ * @author Ava 
+ */
+    
     public Transform(Item p){
         
     }
     
-    //@return a String saying the Item was transformed
+/**
+* This is the execute method that the Transform class must have after extending Event - execute alters the
+* state of an item
+* @return a String saying the Item was transformed
+* @author Ava 
+*/
+    
     public String execute(){
         
     }
