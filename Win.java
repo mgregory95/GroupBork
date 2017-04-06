f7cc384a83db9900afbfe007a470e3a6d41a5156
mgregory95 <mgregory@umw.edu>
2017-04-05 23:22:43 -0400

    Update Win.java

--- Win.java
+++ Win.java
@@ -7,11 +7,21 @@ package borkgroupeventclass;
  */
 public class Win extends Event {
     
+ /**
+ * This is the Win constructor. When the adventurer wins a win event is created. 
+ *
+ * @author Ava Monsouri
+ */
     public Win(){
         
     }
+    /**
+* This is the execute method that the Win class must have after extending Event - execute alters the
+* game to close it down and produces a WINNER! message.
+* @return A String letting the player know that they won
+* @author Ava Monsouri
+*/
     
-    //@return A String letting the player know that they won
     public String execute(){
         
     }
