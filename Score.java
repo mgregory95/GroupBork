850bfb7be06f915abc76441c8de3c9b8002696a4
jfulkers <jfulkerson13@gmail.com>
2017-04-05 23:31:11 -0400

    Update Score.java

--- Score.java
+++ Score.java
@@ -1,24 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package borkgroupeventclass;
+package zeitz_borkv3;
 
 /**
- *
- * @author Ava Mansouri
+ * This class keeps track of the adventurers score and will add or subtract
+ * from it depending on what happens. The user can use a command to stay
+ * up to date with their score throughout the game.
+ * 
+ * @author Ava
  */
-public class Score extends Event {
+class Score extends Event {
     
     private int score; 
     
-    //@param int s is the players score
+    /**
+     * The constructor method for the score event takes in the number of
+     * points to add or subtract from the adventurers score.
+     * 
+     * @param int s is the change in point value
+    */
     public Score(int s){
-        
+        this.score = score + s;
     }
     
-    //@return a String saying that the user has scored and the updated score
+    /**
+     * The execute method lets the user know how many points were added to or
+     * deducted from their score and tells them what their score is after
+     * it has been updated. They can also call it again even if there isn't
+     * a change in the score by using the score command.
+     * 
+     * @return a String saying that the user has scored and the updated score
+    */ 
     public String execute(){
         
     }
