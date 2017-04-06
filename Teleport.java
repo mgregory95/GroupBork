0b0993611a84606ae524489bc486496ca77d8ca2
mgregory95 <mgregory@umw.edu>
2017-04-05 23:31:57 -0400

    Update Teleport.java

--- Teleport.java
+++ Teleport.java
@@ -1,21 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package borkgroupeventclass;
 
+
 /**
- *
- * @author Ava Mansouri
- */
+* The teleport class is an event class. This class extends the abstract class Event. 
+* This class moves the adventurer "magically" to a new location.
+* @author Ava 
+*/
 public class Teleport extends Event {
     
+    
+ /**
+ * This is the Teleport constructor. When the adventurer is teleported a teleport event is created. 
+ * 
+ * @author Ava 
+ */
+
     public Teleport(){
         
     }
-    
-    //@return String saying that the user has teleported 
+/**
+* This is the execute method that the Teleport class must have after extending Event - execute alters the
+* current room of the adventurer
+* @return String saying that the user has teleported 
+* @author Ava 
+*/
+
     public String execute(){
         
     }
