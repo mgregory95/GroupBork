e60f90f1871daf903f1de36f0bd70254e1189051
mgregory95 <mgregory@umw.edu>
2017-04-14 13:52:39 -0400

    Update HealthCommand.java

--- HealthCommand.java
+++ HealthCommand.java
@@ -1,6 +1,5 @@
 
-package zeitz_borkv3;
-
+package GroupBork;
 /**
  * Used when the adventurer commands the system
  * to tell them what their health is currently at. Health
@@ -27,7 +26,37 @@ class HealthCommand extends Command{
      *                  based on how much health he/she has.
      */
     String execute() {
-        return "This is your health.\n";
+        int health = GameState.instance().getHealth();
+        String healthReturn = "";
+        
+        if(health > 0 && health <= 10){
+            healthReturn =  "You are close to death.\n";
+        }
+        else if(health > 10 && health <=20){
+            healthReturn = "You do not feel good at all and need to eat very soon.\n";
+        }
+        else if(health > 20 && health <=30){
+            healthReturn = "You feel very light-headed.\n";
+        }
+        else if(health > 30 && health <=40){
+            healthReturn = "Your stomach grumbles violently.\n";
+        }
+        else if(health > 40 && health <=50){
+            healthReturn = "You are starting to feel a bit fatigued.\n";
+        }
+        else if(health > 50 && health <=60){
+            healthReturn = "Your stomach grumbles lightly.\n";
+        }
+        else if(health > 70 && health <=80){
+            healthReturn = "You feel good and could eat something, but not in a hurry.\n";
+        }
+        else if(health > 80 && health <=90){
+            healthReturn = "You feel good and comfortable.\n";
+        }
+        else if(health > 90 && health <=100){
+            healthReturn = "You feel great!\n";
+        }
+        
+        return healthReturn;
     }
 }
-
