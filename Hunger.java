cd9ce3738e936b3773a6071b28feccaf4b3d542a
mgregory95 <mgregory@umw.edu>
2017-04-23 11:07:00 -0400

    Add files via upload

--- Hunger.java
+++ Hunger.java
@@ -3,74 +3,21 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package groupbork;
+package GroupBork;
 
 /**
  *
- * @author Ava Mansouri
+ * @author Meredith
  */
-public class Hunger {
-    
-    private int numMoveCommands; 
-    private int hunger = 100; 
-    
-    
-    public Hunger(int moveCommands){
-       moveCommands = numMoveCommands; 
+public class Hunger extends Event{
+    private int p;
+    // constructor
+    public Hunger(int p){
+        this.p = p;
     }
-    
-    public void subtractHunger(){
-        hunger --; 
-        GameState.instance().setHunger(hunger); 
-    }
-    
-    public void eat (int foodValue){
-        hunger += foodValue; 
-        GameState.instance().setHunger(hunger); 
-    }
-    
-    public String getHunger(){
-        if (hunger <= 100 && hunger >= 90){
-            System.out.println("Your belly is full!"); 
-        }
-        if (hunger <=89 && hunger >= 80){
-            System.out.println("You feel great!");
-        }
-        if (hunger <= 79 && hunger >= 70){
-            System.out.println("You could eat, but you can stand to wait."); 
-        }
-        if (hunger <=69 && hunger >= 60){
-            System.out.println("Your stomach starts to growl");
-        }
-         if (hunger <=59 && hunger >= 50){
-            System.out.println("Your getting pretty hungry, maybe you should eat"); 
-        }
-        if (hunger <=49 && hunger >= 40){
-            System.out.println("You must eat very soon");
-        }
-        if (hunger <= 39 && hunger >= 30){
-            System.out.println("You are getting very weak, your body can't hold on much longer"); 
-        }
-        if (hunger <=29 && hunger >= 20){
-            System.out.println("You can hardly muster the energy to move, you need to find food immediatley");
-        }
-        if (hunger <=19 && hunger >= 10){
-            System.out.println("Eat now or you will die very soon"); 
-        }
-        if (hunger <=9 && hunger >= 1){
-            System.out.println("You are on the brink of death");
-        }
-        if (hunger == 0){
-            System.out.print("You have died of starvation");
-            //die? 
-        }
-        
-        else {
-            return null; 
-        
-        } 
-        
-        return null; 
+    public String execute(String s){
+        GameState.instance().updateHunger(this.p);
+        return "";
     }
     
 }
