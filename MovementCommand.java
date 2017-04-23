4c5bdfcd659041bfcba4b4d9a8c16c730250ea5a
mgregory95 <mgregory@umw.edu>
2017-04-23 11:02:30 -0400

    Update MovementCommand.java

--- MovementCommand.java
+++ MovementCommand.java
@@ -1,10 +1,12 @@
-package groupbork;
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GroupBork;
 class MovementCommand extends Command {
 
     private String dir;
-    private int numMoves = 0; 
-    
                        
 
     MovementCommand(String dir) {
@@ -12,13 +14,14 @@ class MovementCommand extends Command {
     }
 
     public String execute() {
-        Room currentRoom = GameState.instance().getAdventurersCurrentRoom();
+        GameState gs = GameState.instance();
+        gs.updateNumMoves(1);
+        gs.updateHunger(-1);
+        Room currentRoom = gs.getAdventurersCurrentRoom();
         Room nextRoom = currentRoom.leaveBy(dir);
         if (nextRoom != null) {  // could try/catch here.
-            GameState.instance().setAdventurersCurrentRoom(nextRoom);
-            numMoves++; 
+            gs.setAdventurersCurrentRoom(nextRoom);
             return "\n" + nextRoom.describe() + "\n";
-            
         } else {
             return "You can't go " + dir + ".\n";
         }
